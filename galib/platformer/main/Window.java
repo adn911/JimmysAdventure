e44216316d62f61963f877324cdfa8b53bf8c11b
adn911 <galib.adnan911@gmail.com>
2015-06-26 01:09:59 +0600

    added new lines ..

--- galib/platformer/main/Window.java
+++ galib/platformer/main/Window.java
@@ -7,14 +7,19 @@ public class Window {
 	public static void main(String args[]) {
 
 		JFrame window = new JFrame();
+		
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		window.setTitle("Jimmys Adventure");
 
 		window.setUndecorated(true);
+		
 		window.add(new Canvas());
+		
 		window.pack();
 
 		window.setResizable(false);
+		
 		window.setLocationRelativeTo(null);
 
 		window.setVisible(true);
