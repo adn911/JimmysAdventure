66c24cbe5d1247e071b75a6a351718e872c0877c
adn911 <galib.adnan911@gmail.com>
2015-06-27 14:46:28 +0600

    main

--- galib/platformer/main/Window.java
+++ galib/platformer/main/Window.java
@@ -5,20 +5,15 @@ import javax.swing.JFrame;
 public class Window {
 
 	public static void main(String args[]) {
-
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setTitle("Jimmys Adventure");
-
 		window.setUndecorated(true);
 		window.add(new Canvas());
 		window.pack();
-
 		window.setResizable(false);
 		window.setLocationRelativeTo(null);
-
 		window.setVisible(true);
-		
 	}
 
         public static void methodB(){
